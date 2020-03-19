3a5d23f8f8ac44b520d6a9fe3de4f28f7e3d4515
Paliy Stepan <palstepweb@gmail.com>
2020-03-19 15:38:57 +0300

    Updates

---
+++ precache-manifest.140fbe494c08574cff053badeb5313b9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe695f5bd3c189f824fbac790d973ff",
+    "url": "/Samurai/index.html"
+  },
+  {
+    "revision": "f03b1e6af81bbf7fe364",
+    "url": "/Samurai/static/css/3.2c509ab8.chunk.css"
+  },
+  {
+    "revision": "e2941f5eacce150b0e2b",
+    "url": "/Samurai/static/css/4.f5bfdac4.chunk.css"
+  },
+  {
+    "revision": "c18d66f14818777ee90c",
+    "url": "/Samurai/static/css/5.b690f9e9.chunk.css"
+  },
+  {
+    "revision": "caa054895f5f46021cca",
+    "url": "/Samurai/static/css/main.64f10ca8.chunk.css"
+  },
+  {
+    "revision": "d5e357f5ac0501e387a9",
+    "url": "/Samurai/static/js/2.731c4150.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/Samurai/static/js/2.731c4150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03b1e6af81bbf7fe364",
+    "url": "/Samurai/static/js/3.fab36d20.chunk.js"
+  },
+  {
+    "revision": "e2941f5eacce150b0e2b",
+    "url": "/Samurai/static/js/4.42c40626.chunk.js"
+  },
+  {
+    "revision": "c18d66f14818777ee90c",
+    "url": "/Samurai/static/js/5.52126e70.chunk.js"
+  },
+  {
+    "revision": "caa054895f5f46021cca",
+    "url": "/Samurai/static/js/main.3e08783a.chunk.js"
+  },
+  {
+    "revision": "cfa8e4259326466d0944",
+    "url": "/Samurai/static/js/runtime-main.a21feec1.js"
+  },
+  {
+    "revision": "29d7d247f9ff279af161350a26d61795",
+    "url": "/Samurai/static/media/loading.29d7d247.gif"
+  },
+  {
+    "revision": "1c9f98d164352992a7ab86f8ab682a4b",
+    "url": "/Samurai/static/media/user-img.1c9f98d1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PaliyStepan/Samurai/precache-manifest.74fa2acfef61572a5259ec059ef944b6.js"
+  "/Samurai/precache-manifest.140fbe494c08574cff053badeb5313b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PaliyStepan/Samurai/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Samurai/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
